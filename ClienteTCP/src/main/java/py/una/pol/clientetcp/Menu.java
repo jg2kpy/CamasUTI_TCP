da56d87ec11f725970a8a62e8b845c36794121ea
Jose Luis Junior Gutierrez Aguero <jlgutierrez2000@hotmail.com>
2021-08-23 12:07:14 -0400

    Se agrego clase Menu que sirve de IO para el cliente

---
+++ ClienteTCP/src/main/java/py/una/pol/clientetcp/Menu.java
@@ -0,0 +1,76 @@
+package py.una.pol.clientetcp;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jg2kpy https://github.com/jg2kpy https://juniorgutierrez.com/
+ */
+public class Menu {
+    
+    public static void menuError(String error){
+        System.err.println(error);
+    }
+    
+    public static String nombreHospital;
+    
+    public static String menuLogin(){   
+        System.out.println("Que usuario desea usar?");
+        Scanner sc = new Scanner(System.in);
+        nombreHospital = sc.nextLine();
+        System.out.println("Contrasena:");
+        String pass = sc.nextLine();
+        return nombreHospital + ":" + pass;
+    }
+    
+    public static int[] menuCLI(){
+        
+        int[] retorno = new int[2];
+        
+        System.out.println("Que desea hacer?");
+        System.out.println("1: ver_estado");
+        System.out.println("2: crear_cama");
+        System.out.println("3: eliminar_cama");
+        System.out.println("4: ocupar_cama");
+        System.out.println("5: desocupar_cama");
+        System.out.println("6: desloguear");
+        System.out.println("7: salir");
+        
+        Scanner sc = new Scanner(System.in);
+        int opt = sc.nextInt();
+        
+        retorno[0] = opt;
+        
+        switch (opt) {
+            case 1:
+                System.out.println("Estado actual de: "+nombreHospital);
+                retorno[1] = 0;
+                return retorno;      
+            case 2:
+                System.out.println("Cuantas camas desea agregar?");
+                break;
+            case 3:
+                System.out.println("Cuantas camas desea eliminar?");
+                break;
+            case 4:
+                System.out.println("Que cama desea ocupar?");
+                break;
+            case 5:
+                System.out.println("Que cama desea desocupar?");
+                break;  
+            case 6:
+                System.out.println("Deslogueando de seccion: "+nombreHospital);
+                retorno[1] = -1;
+                return retorno;
+            case 7:
+                System.out.println("Matando proceso cliente");
+                retorno[0] = 6;
+                retorno[1] = -2;
+                return retorno;
+            default:                
+                break;
+        }
+        retorno[1] = sc.nextInt();
+        return retorno;
+    }
+}
